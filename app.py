0f1d1de2da9ca457732cac5ef5a0d2d80a111dfb
maliabarker <89502882+maliabarker@users.noreply.github.com>
2021-11-05 15:50:06 -0700

    user can create new donation

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+from datetime import datetime
 
 client = MongoClient()
 db = client.CharityTracker
@@ -11,11 +12,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-
+    """Landing page (index.html)"""
     return render_template('index.html')
+
+# ————————————————————————————————————————————————————————————————————————
 
 @app.route('/home')
 def home():
+    """Home page (home.html)"""
     return render_template('home.html')
 
 @app.route('/home', methods=['POST'])
@@ -25,19 +29,35 @@ def user_submit():
         'username': request.form.get('username'),
         'password': request.form.get('password')
     }
-    user.insert_one(users)
+    users.insert_one(user)
     print(request.form.to_dict())
     return redirect(url_for('home'))
 
-users = [
-    { 'username': 'malia123', 'password': 'ilovedogs'},
-    { 'username': 'beepboop', 'password': 'ilovecats'}
-]
+# ————————————————————————————————————————————————————————————————————————
 
 @app.route('/donations')
 def donations_index():
     """Show all donations"""
     return render_template('donations_index.html', donations=donations.find())
+
+@app.route('/donations/new')
+def donations_new():
+    """Create new donation"""
+    return render_template('donations_new.html', donation={}, title='New Donation')
+
+@app.route('/donations', methods=['POST'])
+def submit_donation():
+    """Submit a new donation"""
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    donation = {
+        'charity': request.form.get('charity'),
+        'donation_amnt': request.form.get('donation-amnt'),
+        'created_at': dt_string
+    }
+    print(request.form.to_dict())
+    donations.insert_one(donation)
+    return redirect(url_for('donations_index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
